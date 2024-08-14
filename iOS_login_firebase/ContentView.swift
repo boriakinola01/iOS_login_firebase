60307393de7ea67b2541a0b61ed973574397de39
Bori Akinola <boriakinola01@gmail.com>
2024-08-14 10:41:11 +0100

    Initial Commit

---
+++ iOS_login_firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_login_firebase
+//
+//  Created by Bori Akinola on 14/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_login_firebase/iOS_login_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_login_firebaseApp.swift
+//  iOS_login_firebase
+//
+//  Created by Bori Akinola on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_login_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
