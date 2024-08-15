00c1a8c63f81ea6548721d32fe37ce117b1b6a94
Bori Akinola <boriakinola01@gmail.com>
2024-08-15 11:42:21 +0100

    added gitignore

---
+++ iOS_login_firebase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  iOS_login_firebase
+//
+//  Created by Bori Akinola on 15/08/2024.
+//
+
+import Foundation
+
+struct Constants {
+    let FIREBASE_API_KEY = "AIzaSyD1sXWdCHrqIMkGpCFLI2QtzmZpEwh0kvU"
+}
