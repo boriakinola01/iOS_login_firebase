ba50a713f9deded2d570f303e22b375c787659c2
Bori Akinola <boriakinola01@gmail.com>
2024-08-15 11:05:00 +0100

    getting started

---
+++ iOS_login_firebase/ContentViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  ContentViewModel.swift
+//  iOS_login_firebase
+//
+//  Created by Bori Akinola on 14/08/2024.
+//
+
+import Foundation
+
+enum AuthState {
+    
+    case none
+    case authenticated
+    case notAuthenticated
+}
+
+class ContentViewModel: ObservableObject {
+    
+    @Published var authState = AuthState.none
+    
+    
+}
--- iOS_login_firebase/iOS_login_firebaseApp.swift
+++ iOS_login_firebase/iOS_login_firebaseApp.swift
@@ -6,9 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    print("Firebase configured!")
+
+    return true
+  }
+}
 
 @main
 struct iOS_login_firebaseApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
